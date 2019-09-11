70b878f278bee3ddaca70d62adca41100e33961b
Den1809 <denis.v.gusev@megafon.ru>
2019-09-11 13:47:57 +0300

    easy

--- lesson1-easy.py
+++ lesson1-easy.py
@@ -1,10 +1,13 @@
+# Task 1
 str='Привет'
 name = input('Введите имя: ')
 print(str,name)
 
+# Task 2
 number = int(input('Введите число: '))
 print(number+2)
 
+#Task 3
 age = int(input('Введите ваш возраст: '))
 
 if age >= 18:
