706603688e7faf4b048f6278967711e9012a3dee
Den1809 <denis.v.gusev@megafon.ru>
2019-09-11 13:47:41 +0300

    norm

--- lesson1-norm.py
+++ lesson1-norm.py
@@ -1,3 +1,5 @@
+#Task 1
+
 min_number, max_number = 0, 10
 number = int(input('Введите число от ' + str(min_number) + ' до ' + str(max_number) + ': '))
 
@@ -6,3 +8,18 @@ while not (number > min_number and number < max_number):
     number = int(input('Введите число от ' + str(min_number) + ' до ' + str(max_number) + ': '))
 
 print('Результат: ', str(number ** 2))
+
+#Task2
+
+a = int(input('Ввод 1: '))
+b = int(input('Ввод 2: '))
+
+print(a, b)
+
+a, b = b, a
+print(a, b)
+
+a = a + b
+b = a - b
+a = a - b
+print(a, b)
